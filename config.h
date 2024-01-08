a8bdf2f497084f17a6cca7a6b57544e5ce373311
altjp <atljp@t-online.de>
2024-01-08 18:11:05 +0100

    added battery and volume script

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 };
 
